ee66f31017024f54fe7965eb8a12ed365c3f46bb
Sarah Cowgill <sarah.cowgill@yahoo.com>
2020-07-21 12:06:36 -0700

    updating files

---
+++ script.js
@@ -0,0 +1,19 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+    
+
+    const APIKey = "8872e79058967332069d6e666e559915";
+//  When search button is clicked, read the city name typed by the user
+
+   
